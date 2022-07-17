60af34b4409a2fc3613517233188e7969a8e030e
Muzeef <ansarimuzeef@gmail.com>
2022-07-17 16:00:34 +0500

    added calculator files

---
+++ index.js
@@ -0,0 +1,58 @@
+let screen = document.getElementById('screen');
+let buttons = document.querySelectorAll('button');
+let screenValue = '';
+
+for (item  of buttons) {
+    item.addEventListener('click' , (e)=>{
+        buttonText = e.target.innerText;
+        console.log('button text is', buttonText)
+        if (buttonText === 'X'){
+            buttonText = '*'
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+        else if (buttonText === 'C'){
+            screenValue = ""
+            screen.value = screenValue
+        }
+        else if (buttonText === '='){
+            screen.value = eval(screenValue);
+        }
+        else{
+            screenValue += buttonText
+            screen.value = screenValue
+        }
+    })
+}
+
+
+
+
+// var screen = document.getElementById("screen");
+// var buttons = document.querySelectorAll('button');
+// var screenValue = '';
+// for(item of buttons){
+//     item.addEventListener('click',(e)=>{
+//        buttonText = e.target.innerText;
+//         console.log("button text is", buttonText)
+//         if(buttonText == 'X'){
+//             buttonText = '*'
+//             screenValue += buttonText;
+//             screen.value = screenValue;
+//         }
+//         else if(buttonText == "C"){
+//             screenValue = ""
+//             screen.value = screenValue
+//         }
+
+//         else if (buttonText = '='){
+
+//         screen.value = eval(screenValue)
+//         }
+        
+
+//         else{
+//             screenValue += buttonText
+//         }
+//     })
+// }
